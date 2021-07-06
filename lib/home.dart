1d56d8c1c8fd7a13cbd26249b824970fd5c4777c
jp <johannes.p2005@gmail.com>
2021-07-06 21:11:01 +0200

    new vision

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,8 @@ import 'pages/feed.dart';
 import 'pages/search.dart';
 import 'pages/profile.dart';
 import 'pages/post.dart';
+import 'pages/direct.dart';
+
 
 class Home extends StatefulWidget {
   @override
@@ -10,25 +12,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Widget> pages = [Feed(), Search(), Profile(), Post()];
+  List<Widget> pages = [Profile(), Feed(), Direct()];
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 4,
-        initialIndex: 0,
+        length: 3,
+        initialIndex: 1,
         child: Scaffold(
           body: TabBarView(
             children: pages,
           ),
           bottomNavigationBar: Container(
-            margin: EdgeInsets.only(bottom: 20),
+            height: 70,
+            margin: EdgeInsets.only(bottom: 0),
             child: TabBar(
               tabs: [
-                Tab(icon: Icon(Icons.home_filled)),
-                Tab(icon: Icon(Icons.search)),
-                Tab(icon: Icon(Icons.account_circle_rounded)),
-                Tab(icon: Icon(Icons.add_circle_outline_sharp))
+                Tab(icon: Icon(Icons.account_circle_rounded, size: 30,)),
+                Tab(icon: Icon(Icons.circle, size: 25,)),
+                Tab(icon: Icon(Icons.chat_bubble_outline_outlined, size: 25,))
               ],
               unselectedLabelColor: Colors.black,
               labelColor: Colors.blue,
